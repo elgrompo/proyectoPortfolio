823449a36ad83747dc97d6f74e87eadae3de8661
Pablo Ibarbuen <grompborger@gmail.com>
2025-03-07 13:02:51 +0100

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,ts,jsx,tsx}",
+      "./index.html",  // Agregar esto para asegurarte de que index.html también sea procesado
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
